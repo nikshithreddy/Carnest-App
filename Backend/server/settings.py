99034c0d18921ccde8bae3fddcac80ccc7c6d6b4
nikshithreddy <165375024+nikshithreddy@users.noreply.github.com>
2025-04-04 21:11:58 -0700

    Add .env file and update PostgreSQL DB configurations
    - Added .env file containing DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, and DBSCHEMA.
    - Updated settings.py to load DB settings from environment variables.

--- Backend/server/settings.py
+++ Backend/server/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -88,15 +92,15 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 # Local settings for PostgreSQL
 DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": 'carnest',
-            "USER": 'carnestuser',
-            "PASSWORD": 'carnest',
-            "HOST": 'localhost',
-            "PORT": 5432,
-        }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT", 5432),  # Default port to 5432 if not set
     }
+}
 
 
 # Password validation
